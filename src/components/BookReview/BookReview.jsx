e7612ecbf1e49e039bf1b2893f30bf07707b598c
saykot12 <saykot.cse43@gmail.com>
2022-04-04 12:22:22 +0600

    review page updated

--- src/components/BookReview/BookReview.jsx
+++ src/components/BookReview/BookReview.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const BookReview = ({ book }) => {
-  const { bookname, img, comment, rating, author, price, authorImage } = book;
+  const {
+    bookname,
+    img,
+    comment,
+    rating,
+    author,
+    price,
+    authorImage,
+    comment_name,
+  } = book;
   return (
     <div>
       <div className="shadow-lg rounded-2xl w-80 h-100 flex flex-col bg-white p-4 border-4 mt-10">
@@ -15,18 +24,21 @@ const BookReview = ({ book }) => {
           {bookname}
         </p>
         <div className=" justify-between flex items-center gap-1">
-          <div><span className="font-semibold">লেখক: </span>
-          {author}</div>
           <div>
-              <img className="rounded-full h-20 w-20" src={authorImage} alt="" />
+            <span className="font-semibold">লেখক: </span>
+            {author}
           </div>
-
+          <div>
+            <img className="rounded-full h-20 w-20" src={authorImage} alt="" />
+          </div>
         </div>
         <p className="text-start">
           <span className="font-semibold">Comment:</span>
-          {comment}
+          {comment} comment by <span className=" font-bold">{comment_name}</span>
         </p>
-        <p>Rating: <span className=" font-bold">{rating}</span> start</p>
+        <p>
+          Rating: <span className=" font-bold">{rating}</span> start
+        </p>
         <p className="text-start">
           <span className="font-semibold">Price: </span>
           {price}
